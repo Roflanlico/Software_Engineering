82239a4c210338f1212b6a8cd5e656e5eaf5ccbe
Марк Шубат <71442207+MarkShubat@users.noreply.github.com>
2023-12-10 23:04:35 +0500

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -22,7 +22,7 @@ def load_model():
 
 def preprocess_image(img):
     img = img.resize((224, 224))
-    x = image.img_to_array(img)
+    x = Image.img_to_array(img)
     x = keras.applications.vgg16.preprocess_input(img)
     x = np.expand_dims(x, axis=0)
     return x
