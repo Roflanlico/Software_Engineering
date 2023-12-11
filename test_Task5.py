2b8103af51a12de932d532bd047979be702b5c3f
Марк Шубат <71442207+MarkShubat@users.noreply.github.com>
2023-12-12 00:33:11 +0500

    Add files via upload

---
+++ test_Task5.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+from Task3 import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+def test_read_predict_volcano():
+    response = client.post("/predict/", json={"text": "volcano"} )
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data == 980
+
+    
+def test_read_predict_ocean():
+    response = client.post("/predict/", json={"text": "ocean"} )
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data == 392
+
