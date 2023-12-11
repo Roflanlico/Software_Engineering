2e5f51cb3575183540ac0a35498f30ca8f68d5b0
Марк Шубат <71442207+MarkShubat@users.noreply.github.com>
2023-12-12 00:20:17 +0500

    Update Task1.py

--- Программная инженерия/Task1.py
+++ Программная инженерия/Task1.py
@@ -1,14 +1,14 @@
-﻿import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras
 from io import BytesIO
 from PIL import Image
+
 model = keras.applications.VGG16()
 img = Image.open('test_image.jpg')
-plt.imshow( img )
 img = np.array(img)
 x = keras.applications.vgg16.preprocess_input(img)
 x = np.expand_dims(x, axis=0)
-res = model.predict( x )
+res = model.predict(x)
 print(np.argmax(res))
 
